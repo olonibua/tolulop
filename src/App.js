29b54c02ad200c0be4503774c2d2f32d2e06c868
tolulope55 <tolulopeolonibua@gmail.com>
2022-09-17 06:41:36 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,22 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import {Routes, Route, Link} from "react-router-dom";
 import { Themeprovider } from './context/ThemeContext';
-import face from './img/face.png'
 
 import './App.css';
-// import About from './Component/About';
-// import Contact from './Component/Contact';
-// import Home from './Component/Home';
-import Nav from './Component/Nav';
 import Pocket from './routes/Pocket';
-// import Project from './Component/Project';
-// import Skills from './Component/Skills';
-// import face from './img/face.png'
-// import scroll from './img/scroll.png'
-// import whitemode from './img/whitemode.png'
-import Home from './routes/Home';
-
 import Portfolio from './routes/Portfolio';
-import ThemeToggle from './Component/Themetoggle';
-import Contact from './Component/Contact';
+import Home from './Component/Home';
+
 
 
 function App() {
-
-
-
   
-  const [showComponent, setShowComponent] = useState(false);
-
-  useState(() => {
-      setInterval(() => {
-       setShowComponent(true);
-      }, 3700);
-  }, []);
-
   return <Themeprovider>
-     {showComponent &&   <ThemeToggle />}
-
-    
-   
-
-  <Routes>
-      
-       <Route path="/" element={<Home />} />
-       <Route path="pocket" element={<Pocket />} />
-       <Route path="portfolio" element={<Portfolio />} />
-     </Routes>
-     <Home />
-     {showComponent &&  <Contact />}
-   
+      <Routes>
+       <Route path="/" element={<Home />}/>
+       <Route path="/pocket" element={<Pocket />} />
+       <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
 
   </Themeprovider>
 
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -27,8 +27,8 @@ import edu from'../img/edu.png'
 import twitter from '../img/Twitter.png'
 import Github from '../img/github.png'
 import Behance from '../img/behance.png'
-import Nav from '../Component/Nav';
-import ThemeToggle from '../Component/Themetoggle';
+import Nav from './Nav';
+import ThemeToggle from './Themetoggle';
 
 
 
@@ -83,7 +83,7 @@ const Home = (props) => {
     
 
     <div id='home' className='mt-6 sm:py-0'>
-
+ {showComponent &&   <ThemeToggle />}
         
     
 
@@ -308,8 +308,28 @@ const Home = (props) => {
     </div>}
        
 
-
-
+    {showComponent &&   
+    <div className='text-white  bg-black w-full  mx-auto '>
+    <div className='sm:max-w-[500px]  md:max-w-[700px] lg:max-w-[1050px] max-w-[300px] w-full py-10  mx-auto'>
+    <p className='text-md'>Got an idea?</p>
+        <h2 className='text-2xl md:text-3xl lg:text-4xl pb-8 p underline underline-offset-4 font-bold'><a href='https://linkedin.com/olonibua-tolu-465042127' target="_blank">Let's talk about it</a></h2>
+      <div className=' border-b-2 py-2 grid md:grid-cols-3'>
+     
+       <p className='text-[#878787]'>mail:<br/><span className='p text-white'>Olonts@gmail.com</span></p>
+          <p className='text-[#878787]'>call:<br/><span className='p text-white'>+2348149249926</span></p>
+          <p className='text-[#878787] sm:ml-24'>Connect:<br/> <span className=' justify-between flex'>
+          <a href='https://twitter.com/ifetolu5' target="_blank"><img src={twitter} alt='' /></a> <a href='https://github.com/tolulope55/' target="_blank" ><img src={Github} alt='' /></a>
+          <a href='https://behance.net/olonibutolulop' target="_blank"> <img src={Behance} alt='' /></a>
+          </span>
+          </p>
+      </div>
+      
+    </div>
+    <div className='bg-[#111117] w-full text-center h-10 mx-auto'>
+          <p className='text-xs py-3 p md:text-md'>Designed and Development by <strong className='text-white p'> <a href='https://linkedin.com/olonibua-tolu-465042127' target="_blank">Tolulope Olonibua</a></strong></p>
+    </div>
+  </div>
+}
    
         
     
